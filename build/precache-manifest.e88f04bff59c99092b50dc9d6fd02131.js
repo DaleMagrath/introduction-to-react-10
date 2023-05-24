1d2d5541f7147c557b699adfcaf5d6a8ba7f4ed2
Dale Magrath <dmagrath@planview.com>
2023-05-24 01:05:53 -0400

    first commit

---
+++ build/precache-manifest.e88f04bff59c99092b50dc9d6fd02131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53215f3f16192e94d7cbe43dbe660c22",
+    "url": "/introduction-to-react-10/index.html"
+  },
+  {
+    "revision": "2a0265c8fd24926414ff",
+    "url": "/introduction-to-react-10/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "824bbb47023036165805",
+    "url": "/introduction-to-react-10/static/js/2.ac8f4ce9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/introduction-to-react-10/static/js/2.ac8f4ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0265c8fd24926414ff",
+    "url": "/introduction-to-react-10/static/js/main.06c627a8.chunk.js"
+  },
+  {
+    "revision": "7bb4c84d8ca9aedcb78a",
+    "url": "/introduction-to-react-10/static/js/runtime-main.e1bcec8d.js"
+  }
+]);
